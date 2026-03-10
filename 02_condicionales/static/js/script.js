d5bd2b75b331de9a1777e8cd82932059a83dda3c
benjaminquepasa <alexandermaldonado321.0@gmail.com>
2026-03-10 12:59:56 -0300

    avance

--- 02_condicionales/static/js/script.js
+++ 02_condicionales/static/js/script.js
@@ -2,11 +2,7 @@ console.log("Conectado con JS...")
 
 /* Bloque 1: Calentamiento (Lógica Básica)
 1. El Portero Digital
-Crea una variable edad. Si la edad es 18 o más, muestra por consola: "Acceso permitido a la App". Si es menor, muestra: "Acceso denegado: necesitas ser mayor de edad".
-2. Validador de Nombres
-Declara una variable nombre.
-Si el nombre no está vacío, crea un arreglo llamado usuarios y agrega el nombre usando .push().
-Si está vacío, muestra: "Error: El nombre no puede estar en blanco". */
+Crea una variable edad. Si la edad es 18 o más, muestra por consola: "Acceso permitido a la App". Si es menor, muestra: "Acceso denegado: necesitas ser mayor de edad".*/
 
 function porteroDigital() {
 
@@ -16,7 +12,25 @@ function porteroDigital() {
         alert("Acceso permitido a la App.")
     } else if (edad < 18) {
         alert("Acceso denegado: necesitas ser mayor de edad.")
+    } else if (edad >= 120) {
+        alert("Use una edad válida")
     } else {
+        alert("Acceso denegado: necesitas ser mayor de edad")
     }
+}
 
+/* 2. Validador de Nombres
+Declara una variable nombre.
+Si el nombre no está vacío, crea un arreglo llamado usuarios y agrega el nombre usando.push().
+Si está vacío, muestra: "Error: El nombre no puede estar en blanco". */
+
+function validadorNombre() {
+    let nombre = prompt("Ingrese su nombre: ");
+    let usuarios = [];
+
+    if (nombre.length > 0) {
+        alert("")
+    } else if (" ") {
+        alert("El nombre no puede estar en blanco.")
+    }
 }
